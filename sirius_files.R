de4a009d8f1735a238ba1a0aa59732544404cc46
Mar Garcia-Aloy <mar.garcia.aloy@gmail.com>
2021-01-28 17:59:30 +0100

    additional annotations

--- sirius_files.R
+++ sirius_files.R
@@ -6,7 +6,8 @@ library(Spectra)
 library(CompoundDb)
 
 db <- read.csv("database.csv")
-db$adduct <- gsub("M-H-hexose]", "M-H-Hexose-H2O]", db$adduct)
+db$adduct <- gsub("H-H2O-CO]", "H-HCOOH]", db$adduct)
+db$adduct <- gsub("H-hexose]", "H-Hexose-H2O]", db$adduct)
 db$adduct <- gsub("\\(H2O)3-C2H2", "\\H2O-H2O-C2H4O (McLafferty)", db$adduct)
 i <- nrow(db)
 
@@ -28,12 +29,21 @@ if(c_add == "[M-H-CH3]-"){
 } else if(c_add == "[M-H-H2O-C2O2]-"){
   c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M-H-H2O]-")) - 
     getMolecule("C2O2")$exactmass
+} else if(c_add == "[M+H-H2O-C2H2O-C2HNO]+"){
+  c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M+H-H2O]+")) - 
+    getMolecule("C2H2O")$exactmass - getMolecule("C2HNO")$exactmass
 } else if(c_add == "[M+H-(H2O)2-CO]+"){
   c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M+H-(H2O)2]+")) - 
     getMolecule("CO")$exactmass
+} else if(c_add == "[M+H-(H2O)2-CO-C5H4N2O]+"){
+  c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M+H-(H2O)2]+")) - 
+    getMolecule("CO")$exactmass - getMolecule("C5H4N2O")$exactmass
 } else if(c_add == "[M-H-(H2O)2-CO2]-"){
   c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M-H-(H2O)2]-")) - 
     getMolecule("CO2")$exactmass
+} else if(c_add == "[M+H-(H2O)2-C2H2O]+"){
+  c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M+H-(H2O)2]+")) - 
+    getMolecule("C2H2O")$exactmass
 } else if(c_add == "[M-H-(H2O)2-C3H2O]-" | c_add == "[M-H-(H2O)2-CO-C2H2]-"){
   c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M-H-(H2O)2]-")) - 
     getMolecule("C3H2O")$exactmass
@@ -52,6 +62,12 @@ if(c_add == "[M-H-CH3]-"){
 } else if(c_add == "[M-H-hexose-H2O]-"){
   c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M-H-Hexose-H2O]-")) - 
     getMolecule("H2O")$exactmass
+} else if(c_add == "[M+H-hexose-H2O]+"){
+  c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M+H-Hexose-H2O]+")) - 
+    getMolecule("H2O")$exactmass
+} else if(c_add == "[M+H-hexose-(H2O)2-CH2]+"){
+  c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M+H-Hexose-H2O]+")) - 
+    getMolecule("H2O")$exactmass*2 - getMolecule("CH2")$exactmass
 } else if(c_add == "[M-H-(hexose)2]-"){
   c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M-H]-")) - 
     getMolecule("C6H10O5")$exactmass*2
@@ -61,6 +77,9 @@ if(c_add == "[M-H-CH3]-"){
 } else if(c_add == "[M+H-C2H5NO3]+"){
   c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M+H]+")) - 
     getMolecule("C2H5NO3")$exactmass
+} else if(c_add == "[M+H-H3NO-CH2N2]+"){
+  c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, "[M+H]+")) - 
+    getMolecule("H3NO")$exactmass - getMolecule("CH2N2")$exactmass
 } else {
   c_mz <- unlist(mass2mz(getMolecule(c_fml)$exactmass, c_add))
 }
@@ -69,7 +88,7 @@ xdata <- readMSData(files = paste0("mzML/", db$path[i], "/", db$file[i], ".mzML"
                     mode = "onDisk")
 chr <- chromatogram(xdata, mz = c_mz + 0.01 * c(-1, 1))
 chromPeaks(findChromPeaks(chr, param = CentWaveParam(peakwidth = c(2, 20))))
-c_rt <- 215.5384 
+c_rt <- 78.1819 
 dev.off()
 plot(chr, xlim = c(c_rt - 50, c_rt + 50))
 abline(v = c_rt)
@@ -99,3 +118,4 @@ write.table(sps, paste0("sirius/", db$abr[i], "_", db$polarity[i], "_MS2.txt"),
 plotSpectra(ms2list, #main = sps$name,
             labels = function(z) format(mz(z)[[1L]], digits = 4),
             labelSrt = -30, labelPos = 2, labelOffset = 0.1)
+
