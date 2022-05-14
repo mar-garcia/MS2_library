511b7503d07f14699b113056932b2b0ed7c0e6a4
Mar Garcia Aloy <mar.garcia.aloy@gmail.com>
2022-05-14 20:54:09 +0200

    legumes - stds varis - PLASMA +

--- deleteSpectra.R
+++ deleteSpectra.R
@@ -1,4 +1,5 @@
 library(CompoundDb)
 (cdb <- CompDb("CompDb_inhouse_0.sqlite", flags = RSQLite::SQLITE_RW))
 sps <- Spectra(cdb)
-deleteSpectra(cdb, sps$spectrum_id[which(sps$compound_id == "M0028" & sps$adduct == "[M+CHO2-CO2]-")])
+deleteSpectra(cdb, sps$spectrum_id[which(sps$compound_id == "M0004" & sps$adduct == "[M-H]-")])
+
